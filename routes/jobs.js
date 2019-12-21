b6947a34cae301bb54a0b27b162f2a120dfa1bae
Josh Mabry <mabry1985@gmail.com>
2019-12-20 16:47:55 -0800

    add names to json object

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,49 +8,46 @@ router.route("/").get((req, res) => {
 });
 
 router.route("/react").get((req, res) => {
-  Job.find({ description: /React/})
-  .then(jobs => res.json(jobs))
+Job.find({ description: /React/ })
+  .then(jobs => res.json([jobs, { name: "React" }]))
   .catch(err => res.status(400).json("Error " + err)); 
 });
 
 router.route("/vue").get((req, res) => {
-  Job.find({ description: /Vue/})
-  .then(jobs => res.json(jobs))
-  .catch(err => res.status(400).json("Error " + err)); 
+  Job.find({ description: /Vue/ })
+    .then(jobs => res.json([jobs, { name: "Vue" }]))
+    .catch(err => res.status(400).json("Error " + err)); 
 });
 
 router.route("/angular").get((req, res) => {
-  Job.find({ description: /Angular/})
-  .then(jobs => res.json(jobs))
-  .catch(err => res.status(400).json("Error " + err)); 
+  Job.find({ description: /Angular/ })
+    .then(jobs => res.json([jobs, { name: "Angular" }]))
+    .catch(err => res.status(400).json("Error " + err)); 
 });
 
 router.route("/backbone").get((req, res) => {
-  Job.find({ description: /Backbone/})
-  .then(jobs => res.json(jobs))
-  .catch(err => res.status(400).json("Error " + err)); 
+  Job.find({ description: /Backbone/ })
+    .then(jobs => res.json([jobs, { name: "Backbone" }]))
+    .catch(err => res.status(400).json("Error " + err)); 
 });
 
 router.route("/rails").get((req, res) => {
-  Job.find({ description: /Rails/})
-  .then(jobs => res.json(jobs))
-  .catch(err => res.status(400).json("Error " + err)); 
+  Job.find({ description: /Rails/ })
+    .then(jobs => res.json([jobs, { name: "Rails" }]))
+    .catch(err => res.status(400).json("Error " + err)); 
 });
 
-
-
 router.route("/senior").get((req, res) => {
-  Job.find({ isSenior: true})
-  .then(jobs => res.json(jobs))
-  .catch(err => res.status(400).json("Error " + err)); 
+  Job.find({ isSenior: true })
+    .then(jobs => res.json([jobs, { name: "Senior" }]))
+    .catch(err => res.status(400).json("Error " + err)); 
 });
 
 router.route("/junior").get((req, res) => {
-  Job.find({ isJunior: true})
-  .then(jobs => res.json(jobs))
-  .catch(err => res.status(400).json("Error " + err)); 
+  Job.find({ isJunior: true })
+    .then(jobs => res.json([jobs, { name: "Junior" }]))
+    .catch(err => res.status(400).json("Error " + err)); 
 });
-
 
 router.route("/:id").get((req, res) => {
   Job.findById(req.params.id)
