7f882c083879c0a3dd452f54882a7f1209b97c38
Josh Mabry <mabry1985@gmail.com>
2020-01-05 10:22:33 -0800

    remove unused routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,48 +7,6 @@ router.route("/").get((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.route("/react").get((req, res) => {
-Job.find({ description: /React/ })
-  .then(jobs => res.json([jobs, { name: "React" }]))
-  .catch(err => res.status(400).json("Error " + err)); 
-});
-
-router.route("/vue").get((req, res) => {
-  Job.find({ description: /Vue/ })
-    .then(jobs => res.json([jobs, { name: "Vue" }]))
-    .catch(err => res.status(400).json("Error " + err)); 
-});
-
-router.route("/angular").get((req, res) => {
-  Job.find({ description: /Angular/ })
-    .then(jobs => res.json([jobs, { name: "Angular" }]))
-    .catch(err => res.status(400).json("Error " + err)); 
-});
-
-router.route("/backbone").get((req, res) => {
-  Job.find({ description: /Backbone/ })
-    .then(jobs => res.json([jobs, { name: "Backbone" }]))
-    .catch(err => res.status(400).json("Error " + err)); 
-});
-
-router.route("/rails").get((req, res) => {
-  Job.find({ description: /Rails/ })
-    .then(jobs => res.json([jobs, { name: "Rails" }]))
-    .catch(err => res.status(400).json("Error " + err)); 
-});
-
-router.route("/senior").get((req, res) => {
-  Job.find({ isSenior: true })
-    .then(jobs => res.json([jobs, { name: "Senior" }]))
-    .catch(err => res.status(400).json("Error " + err)); 
-});
-
-router.route("/junior").get((req, res) => {
-  Job.find({ isJunior: true })
-    .then(jobs => res.json([jobs, { name: "Junior" }]))
-    .catch(err => res.status(400).json("Error " + err)); 
-});
-
 router.route("/:id").get((req, res) => {
   Job.findById(req.params.id)
     .then(job => res.json(job))
