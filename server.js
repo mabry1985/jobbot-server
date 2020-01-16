283d67b4330293c3606ac2e3b00937fe2d5c8c5b
Josh Mabry <mabry1985@gmail.com>
2020-01-16 10:13:36 -0800

    add config for production env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require('path')
+
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +28,15 @@ app.use("/search", searchRouter);
 app.use("/jobs", jobRouter);
 app.use
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html');
+  })
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
